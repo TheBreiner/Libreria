36eba354155834f7fb21e9e33e07544bd85790b0
TheBreiner <112511374+TheBreiner@users.noreply.github.com>
2022-09-08 12:05:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//clase principal
+public class Libro 
+// atributos
+{ private int ISBN,páginas;
+private string Titulo="",autor="";
+   public void gettitulo(){
+        Console.WriteLine(this.Titulo);
+    }
+public void settitulo(string nombrel){
+        this.Titulo=nombrel;
+}
+    public void getautor(){
+        Console.WriteLine(this.autor);
+    }
+public void setautor(string nombrea){
+        this.autor=nombrea;
+}
+    public void getIBSN(){
+        Console.WriteLine(this.ISBN);
+    }
+public void setIBSN(int ncode){
+        this.ISBN=ncode;
+}
+    public int getnpages(){
+        return this.páginas;
+    }
+public void setnpages(int npagi){
+        this.páginas=npagi;
+}     
+   
+  public void tostring(){ Console.WriteLine("El libro "+this.Titulo+" con ISBN "+this.ISBN+" creado por el autor "+this.autor+" tiene "+this.páginas+" páginas.");
+  }
+}
